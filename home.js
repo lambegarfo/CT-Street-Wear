267b1ebd4101c852e8dc86801ae1e058a28ea35b
lambegarfo <vitor_hugo.f.s@hotmail.com>
2022-11-16 16:41:33 -0300

    Bug pause

--- home.js
+++ home.js
@@ -1,9 +1,15 @@
 import fotos from "./fotos.json" assert {type: 'json'};
 
-const arrayfotos = fotos;
-let divFotos = document.getElementById('foto1');
+const arrayfotos = fotos.fotosGiovana;
+const divFotos = document.getElementById('foto1');
 
-divFotos.style.backgroundImage = "url($(arrayfotos.fotosGiovana.foto1))";
-divFotos.style.backgroundSize = "cover";
 
-console.log(arrayfotos.fotosGiovana.foto1);
+    var i = 0;
+
+    const foto = Object.values(arrayfotos)[i];
+    divFotos.style.backgroundImage = "url(" + foto + ")";
+    divFotos.style.backgroundSize = "cover";
+    divFotos.style.backgroundPosition = "center center";
+
+
+        
